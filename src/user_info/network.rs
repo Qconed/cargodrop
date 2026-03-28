d9dac50e8fe6b4f0f4d0bd9ae898859508757fe4
Matheo Revel <matheorevel17@gmail.com>
2026-03-28 15:05:27 +0100

    fix: remove unused get_local_ip_by_hostname alternative, keep reliable UDP socket approach

--- src/user_info/network.rs
+++ src/user_info/network.rs
@@ -9,21 +9,3 @@ pub fn get_local_ip() -> Result<IpAddr, Box<dyn std::error::Error>> {
     let local_addr = socket.local_addr()?;
     Ok(local_addr.ip())
 }
-
-/// Alternative: détecte l'IP en utilisant le hostname
-/// (moins fiable que get_local_ip)
-#[allow(dead_code)]
-pub fn get_local_ip_by_hostname() -> Result<IpAddr, Box<dyn std::error::Error>> {
-    use std::net::ToSocketAddrs;
-    
-    let hostname = hostname::get()?
-        .into_string()
-        .map_err(|_| "Impossible de convertir le hostname")?;
-    
-    let socket_addr = format!("{}:0", hostname)
-        .to_socket_addrs()?
-        .next()
-        .ok_or("Impossible de résoudre le hostname")?;
-    
-    Ok(socket_addr.ip())
-}
